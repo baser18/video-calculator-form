8013b9a095e084a4b7a842c1a17ea208e56d0019
baser18 <baser930@gmail.com>
2022-09-04 21:31:28 +0100

    removing something not needed

--- src/DigitButton.js
+++
@@ -1,5 +0,0 @@
-import { ACTIONS } from "./App"
-
-export default function DigitButton({dispatch, digit}){
-    return <input onClick={() => dispatch({type: ACTIONS.ADD_DAYS,payload: {digit}})}>{digit}</input>
-}
